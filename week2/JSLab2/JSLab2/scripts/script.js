7cb425ac3808dac7582dfa08cf40559f6432f6c8
cumareshan27 <cumareshan@yahoo.com>
2021-08-14 22:29:28 -0400

    added the initial HTML push

---
+++ week2/JSLab2/JSLab2/scripts/script.js
@@ -0,0 +1,56 @@
+//Add your work to this file.
+
+/* Example
+This is a variable declaration. 
+Also referred to as a variable definition.
+It typically will have a keyword and then the name 
+of the variable. In this case, the variable is named 
+firstVariable. The keyword let indicates to JavaScript
+that we are defining a new variable.*/
+let firstVariable;
+
+/* Your turn 
+Define a variable named data */
+
+/* Example
+ Here we initialize firstVariable
+ In other words we assign it a value for the first
+ time. */
+firstVariable = 100;
+
+/* Now we can use our variable. 
+ Let's print it to the console.
+ 
+ Notice that to use our variable we don't need to
+ use the keyword let again. We just use the name of 
+ the variable.*/
+console.log(firstVariable);
+
+/*Your turn
+ Initialize data to 5000*/
+const secondVariable = 5000;
+
+/*Then print your variable to the console.*/
+
+console.log("Second Varibale " + secondVariable);
+
+const constVariable = "cant be changed";
+
+console.log("Const variables caan't be changed " + constVariable);
+
+firstVariable = "pizza";
+console.log("first variable updated " + firstVariable);
+
+//const variables can't be changed.
+//constVariable = "Reassignment";
+
+var myVar;
+myVar = true;
+myVar = false;
+console.log("My var " + myVar);
+
+console.log(varVariable);
+console.log(abc);
+
+let abc;
+var varVariable;
---
+++ week2/Lab1/script.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log("My name is Kumar /n");
+console.log("My favorite cusine is Italian/n");
+console.log("I am part of the spark");
+
+alert("My name is Kumar");
+alert("My favorite cusine is Italian");
+alert("I am part of the spark");
